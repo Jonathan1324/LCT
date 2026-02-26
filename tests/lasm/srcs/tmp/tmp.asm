2a2f938570278058e4eebe04b1d339b796d54151
Jonathan <mohrjonathan09@gmail.com>
2026-02-26 18:04:36 +0100

    b

--- tests/lasm/srcs/tmp/tmp.asm
+++ tests/lasm/srcs/tmp/tmp.asm
@@ -1,23 +1,55 @@
 ; FORMATS: BIN,ELF
-; BITS: 64
+; BITS: 32
 ; EXPECT: SUCCESS
 
-[bits 64]
+[bits 32]
 
 section .text
     global _start
 
 _start:
-    jmp ld2
-
-    nop
-    nop
-    nop
-L2:
-    hlt
-
-section .data
-
-ld1 resq 51
-
-ld2 dq 8, 2
+    mul dword [bp]
+;
+;    div dl
+;    div cx
+;    div ebx
+;    div byte [eax * 2 + _start]
+;    div word [ebp]
+;    div dword [eax + 10]
+;    div byte [bx + _start]
+;    div word [bp]
+;    div dword [bx + si + 10]
+;
+;    idiv al
+;    idiv di
+;    idiv ecx
+;    idiv byte [eax * 2 + _start]
+;    idiv word [ebp]
+;    idiv dword [eax + 10]
+;    idiv byte [bx + _start]
+;    idiv word [bp]
+;    idiv dword [bx + si + 10]
+;
+;    imul ah
+;    imul dx
+;    imul edx
+;    imul byte [eax * 3 + _start]
+;    imul word [ecx + esp]
+;    imul dword [eax + 15]
+;    imul byte [bx + _start]
+;    imul word [si + bp]
+;    imul dword [bx + si + 15]
+;
+;    imul di, ax
+;    imul eax, edi
+;    imul cx, [edi + esi]
+;    imul eax, [1000]
+;    imul di, [bx + si]
+;    imul ax, [bp]
+;
+;    imul di, ax, 128
+;    imul eax, edi, 74567
+;    imul cx, [edi + esi], -9
+;    imul eax, [1000], -1342
+;    imul di, [bx + si], 6
+;    imul ax, [bp], 1
