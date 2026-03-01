c309b241f8b06908a377874dc4310451abbed2dd
Jonathan <mohrjonathan09@gmail.com>
2026-03-01 15:05:44 +0100

    abc

--- src/tools/lctc/main.rs
+++ src/tools/lctc/main.rs
@@ -1,6 +1,26 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 use std::env;
+use std::process;
+
+pub mod c;
+
+fn print_help() {
+
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("Args: {:?}", args);
+    for arg in env::args().skip(1) {
+        match arg.as_str() {
+            "-v" | "--version" => {
+                unsafe { c::printVersion(); }
+                process::exit(0);
+            }
+            "-h" | "--help" => {
+                print_help();
+                process::exit(0);
+            }
+            _ => {},
+        }
+    }
 }
